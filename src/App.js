682416833f3f9109fdec309cf30c2dbf2bc2c3c5
Cristóvão Antunes <cristovao.av.antunes@gmail.com>
2022-02-18 20:48:11 +0000

    add base pages and styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+//Components
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
+
+//Pages
+import Home from "./pages/Home";
+import About from "./pages/About";
+import NotFound from "./pages/NotFound";
+
 function App() {
   return (
-    <div className="bg-purple-500">
-      <h1 className="text-xl">Hello world</h1>
-      <button className="btn">Click</button>
-    </div>
+    <Router>
+      <div className="flex flex-col justify-between h-screen">
+        <Navbar />
+
+        <main className="container mx-auto px-3 pb-12">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/*" element={<NotFound />} />
+          </Routes>
+        </main>
+
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
